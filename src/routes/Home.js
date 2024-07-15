d95aa669f02f19693f015905cf56b8afc6292369
Turya Biswas <78296735+turyabiswas233@users.noreply.github.com>
2024-07-16 00:48:35 +0600

    update UI bug

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import HeaderElement from '../components/HeaderElement';
+import React from 'react'; 
 import HeroSection from '../components/HeroSection';
 import Specials from '../components/Specials';
  
 const Home = () => {
   return (
     <>
-      <HeaderElement />
       <HeroSection />
       <Specials />
     </>
